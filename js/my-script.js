b898b268f13ca0ee94c466f238e5f0e2eec187e6
sheikhqumrunnessa <sheikhqumrunnessa@gmail.com>
2017-12-14 13:14:43 +0600

    new index.html file

---
+++ js/my-script.js
@@ -0,0 +1,146 @@
+
+$('#img1').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+$('#img2').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+$('#img3').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+$('#img4').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('btn').onclick=function () {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//
+//     newHeadingElement.innerHTML = "This is a heading";
+//     newParagraphElement.innerHTML = "This is a paragraph";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+//
+// }
+
+// function createHeadingParagraphElement() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//
+//     newHeadingElement.innerHTML = "This is a heading";
+//     newParagraphElement.innerHTML = "This is a paragraph";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+//
+// }
+//
+// document.getElementById('btn').onclick=function () {
+//   createHeadingParagraphElement();
+// };
+
+
+
+
+
+
+
+// var newHeadingElement = document.createElement('h1');
+// var newParagraphElement = document.createElement('p');
+//
+//
+// newHeadingElement.innerHTML = "This is a heading";
+// newParagraphElement.innerHTML = "This is a paragraph";
+//
+// document.getElementById('div').appendChild(newHeadingElement);
+// document.getElementById('div').appendChild(newParagraphElement);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#firstName').keyup(function () {
+//     var firstNameValue =$('#firstName').val();
+//     $('#res1').val(firstNameValue);
+// });
+// $('#lastName').keyup(function () {
+//     var lastNameValue =$('#lastName').val();
+//     $('#res2').val(lastNameValue);
+// });
+// $('#lastName').blur(function () {
+//     var firstNameValue =$('#firstName').val();
+//     var lastNameValue =$('#lastName').val();
+//     $('#res3').val(firstNameValue+' '+lastNameValue);
+// });
+
+
+
+
+
+// $('#firstName').keyup(function () {     ////if html a input  na dea hoy
+//     var firstNameValue =$('#firstName').val();
+//     $('#res1').text(firstNameValue);
+// });
+// $('#lastName').keyup(function () {
+//     var lastNameValue =$('#lastName').val();
+//     $('#res2').text(lastNameValue);
+// });
+// $('#lastName').blur(function () {
+//     var firstNameValue =$('#firstName').val();
+//     var lastNameValue =$('#lastName').val();
+//     $('#res3').text(firstNameValue+' '+lastNameValue);
+// });
+
+
+// $('#firstName').keyup(function () {
+//     alert('test');
+//  });
+// $('#firstName').click(function () {
+//     alert('test');
+// });
+// $('#firstName').mouseover(function () {
+//     alert('test');
+// });
